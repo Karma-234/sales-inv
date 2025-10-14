19cc0f815ad5a7040dd5c46b885fb5c72a3e7329
Karma-234 <busari1kamal@gmail.com>
2025-10-14 19:01:14 +0100

    Define product model

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use axum::Router;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+
+    let app = Router::new();
+
+    // Define IP and listener
+    let address = "0.0.0.0:7777";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+
+    // Serve axum app
+    axum::serve(listener, app);
 }
---
+++ src/product/models.rs
@@ -0,0 +1,6 @@
+struct Product {
+    name: String,
+    price: f64,
+    quantity: u32,
+    pack_price: f64,
+}
